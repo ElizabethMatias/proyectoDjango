b9f3b75ef15d734ccc5f6f6eba8bc7a8ef172fba
LirioMendoza <aketzalirio@gmail.com>
2022-07-15 13:48:44 -0500

    Corrección de tipos de datos en modelos y configuración del admin de la aplicación inventario

--- tienda/inventario/admin.py
+++ tienda/inventario/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Producto, VentaProducto, Venta
+
+admin.site.register(Producto)
+admin.site.register(VentaProducto)
+admin.site.register(Venta)
--- tienda/inventario/models.py
+++ tienda/inventario/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=200)
-    precioUnidad = models.DecimalField(default=0)
+    precioUnidad = models.DecimalField(default=0, max_digits=7, decimal_places=2)
     cantidadExistencia = models.IntegerField(default=0)
-    IVAproducto = models.DecimalField(default=0)
+    IVAproducto = models.DecimalField(default=0, max_digits=7, decimal_places=2)
 
 
 class Venta(models.Model):
@@ -18,15 +18,16 @@ class Venta(models.Model):
     tipoPago = models.CharField(
         choices = CHOICE,
         default = 'EF',
+        max_length=200,
     )
-    subTotal = models.DecimalField(default=0)
+    subTotal = models.DecimalField(default=0, max_digits=7, decimal_places=2)
     vendedor = models.CharField(max_length=200)
-    IVAtotal = models.IntegerField(default=0)
-    total = models.IntegerField(default=0)
+    IVAtotal = models.DecimalField(default=0, max_digits=7, decimal_places=2)
+    total = models.DecimalField(default=0, max_digits=7, decimal_places=2)
 
 class VentaProducto(models.Model):
     unidadesTotales = models.IntegerField(default=1)
-    precioUnidades = models.DecimalField(default=0)
-    IVAtotal = models.DecimalField(default=0)
+    precioUnidades = models.DecimalField(default=0, max_digits=7, decimal_places=2)
+    IVAtotal = models.DecimalField(default=0,max_digits=7, decimal_places=2)
     venta = models.ForeignKey(Venta, on_delete=models.CASCADE)
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
